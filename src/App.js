4aefa82de1faeac245c94a1cc38f0d579486b6c5
Anastasia Haller <anadana87@gmail.com>
2023-08-25 12:05:36 +0200

    start project

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import JokeList from "./components/JokeList";
+import "./App.css";
 
 function App() {
+  const dummyJokes = [
+    {
+      id: 1,
+      type: "general",
+      setup: "What do you call a bee that lives in America?",
+      punchline: "A USB.",
+    },
+    {
+      id: 2,
+      type: "programming",
+      setup: "What's the best thing about a Boolean?",
+      punchline: "Even if you're wrong, you're only off by a bit.",
+    },
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <section>
+        <button>Fetch Jokes</button>
+      </section>
+      <section>
+        <JokeList jokes={dummyJokes} />
+      </section>
+    </React.Fragment>
   );
 }
 
